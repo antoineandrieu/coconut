26f06b89d53c30963b456a28092f103b48f87270
Coding Ant <antoine@codingant.io>
2022-02-08 17:46:32 +0100

    fix(contracts): missing dotenv require in hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,6 +5,8 @@ import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 
+require('dotenv').config();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
